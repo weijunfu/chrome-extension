97bc8128ff95a40136848d0cbc3d351d922ead3e
ijunfu <ijunfu@163.com>
2023-07-04 23:49:41 +0800

    fix progess

--- Chapter01-Basic/asserts/background.js
+++ Chapter01-Basic/asserts/background.js
@@ -1,21 +1,26 @@
 const iconUrl = chrome.runtime.getURL("asserts/icons/128.png");
 
+let options = {
+    type: 'progress',
+    iconUrl: iconUrl,
+    title: 'Progress',
+    message: 'Task in progress...',
+    progress: 0,
+}
+
+let notificationsId = 'ijunfu-notification';
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.type === 'show_notification') {
-        chrome.notifications.create({
-            type: 'progress',
-            iconUrl: iconUrl,
-            title: 'Progress',
-            message: 'Task in progress...',
-            progress: message.progress,
-        });
-    }
-});
 
-chrome.runtime.onMessageExternal.addListener(function (message, sender, sendResponse) {
-    if (message.type === 'update_notification') {
-        chrome.notifications.update('progress_notification', {
-            progress: message.progress,
+        options.progress = message.progress
+
+        chrome.notifications.create(notificationsId, options,(res)=>{
+            console.log('res create', res)
         });
+    } else if(message.type === 'update_notification') {
+        options.progress = message.progress
+
+        chrome.notifications.update(notificationsId, options, res => console.log(res));
     }
 });
